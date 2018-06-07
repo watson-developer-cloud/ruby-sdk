6415ae8874903f2594da2c63ea7d2eced5b9018a
Max Nussbaum <maxnussbaum@live.com>
2018-06-07 16:41:41 -0400

    test(assistant): Add unit tests for assistant

--- tests/unit/test_assistant_v1.rb
+++ tests/unit/test_assistant_v1.rb
@@ -45,4 +45,133 @@ class AssistantV1Test < Minitest::Test
       assert(expected_response.headers[key] == service_response.headers[key])
     end
   end
+
+  def test_rate_limit_exceeded
+    error_code = 429
+    error_msg = "Rate limit exceeded"
+    headers = {
+      "Content-Type" => "application/json"
+    }
+    error_response = {
+      "code" => error_code,
+      "error" => error_msg
+    }
+    stub_request(:post, "https://gateway.watsonplatform.net/assistant/api/v1/workspaces/boguswid/counterexamples?version=2018-02-16")
+      .with(
+        body: "{\"text\":\"I want financial advice today.\"}",
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Content-Type" => "application/json",
+          "Host" => "gateway.watsonplatform.net:443"
+        }
+      ).to_return(status: 429, body: error_response.to_json, headers: headers)
+    service = AssistantV1.new(
+      username: "username",
+      password: "password",
+      version: "2018-02-16"
+    )
+    begin
+      service.create_counterexample(
+        workspace_id: "boguswid",
+        text: "I want financial advice today."
+      )
+    rescue WatsonApiException => e
+      assert(e.code == error_code)
+      assert(e.error == error_msg)
+    end
+  end
+
+  def test_unknown_error
+    error_msg = "Unknown error"
+    error_code = 407
+    headers = {
+      "Content-Type" => "application/json"
+    }
+    error_response = {
+      "error" => error_msg,
+      "code" => error_code
+    }
+    stub_request(:post, "https://gateway.watsonplatform.net/assistant/api/v1/workspaces/boguswid/counterexamples?version=2018-02-16")
+      .with(
+        body: "{\"text\":\"I want financial advice today.\"}",
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Content-Type" => "application/json",
+          "Host" => "gateway.watsonplatform.net:443"
+        }
+      ).to_return(status: 407, body: error_response.to_json, headers: headers)
+    service = AssistantV1.new(
+      username: "username",
+      password: "password",
+      version: "2018-02-16"
+    )
+    begin
+      service.create_counterexample(
+        workspace_id: "boguswid",
+        text: "I want financial advice today."
+      )
+    rescue WatsonApiException => e
+      assert(e.code == error_code)
+      assert(e.error == error_msg)
+    end
+  end
+
+  def test_delete_counterexample
+    headers = {
+      "Content-Type" => "application/json"
+    }
+    stub_request(:delete, "https://gateway.watsonplatform.net/assistant/api/v1/workspaces/boguswid/counterexamples/I+want+financial+advice+today?version=2018-02-16")
+      .with(
+        body: "{}",
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Content-Type" => "application/json",
+          "Host" => "gateway.watsonplatform.net:443"
+        }
+      ).to_return(status: 200, body: {}.to_json, headers: headers)
+    service = AssistantV1.new(
+      username: "username",
+      password: "password",
+      version: "2018-02-16"
+    )
+    service_response = service.delete_counterexample(
+      workspace_id: "boguswid",
+      text: "I want financial advice today"
+    )
+    assert(service_response.nil?)
+  end
+
+  def test_get_counterexample
+    response = {
+      "text" => "What are you wearing?",
+      "created" => "2016-07-11T23:53:59.153Z",
+      "updated" => "2016-12-07T18:53:59.153Z"
+    }
+    headers = {
+      "Content-Type" => "application/json"
+    }
+    stub_request(:get, "https://gateway.watsonplatform.net/assistant/api/v1/workspaces/boguswid/counterexamples/What+are+you+wearing%3F?version=2018-02-16")
+      .with(
+        body: "{}",
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Content-Type" => "application/json",
+          "Host" => "gateway.watsonplatform.net:443"
+        }
+      ).to_return(status: 200, body: response.to_json, headers: headers)
+    service = AssistantV1.new(
+      username: "username",
+      password: "password",
+      version: "2018-02-16"
+    )
+    service_response = service.get_counterexample(
+      workspace_id: "boguswid",
+      text: "What are you wearing?"
+    )
+    assert(service_response.body == response)
+  end
 end
