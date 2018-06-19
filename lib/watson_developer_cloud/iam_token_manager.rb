2a0bfe2c0eae9354fb73201e92f66856d4ba8f83
Max Nussbaum <maxnussbaum@live.com>
2018-06-19 16:25:43 -0400

    feat(authentication): Add IAM authentication support

---
+++ lib/watson_developer_cloud/iam_token_manager.rb
@@ -0,0 +1,166 @@
+# frozen_string_literal: true
+
+require("http")
+require("json")
+require_relative("./version.rb")
+
+DEFAULT_IAM_URL = "https://iam.bluemix.net/identity/token"
+CONTENT_TYPE = "application/x-www-form-urlencoded"
+ACCEPT = "application/json"
+DEFAULT_AUTHORIZATION = "Basic Yng6Yng="
+REQUEST_TOKEN_GRANT_TYPE = "urn:ibm:params:oauth:grant-type:apikey"
+REQUEST_TOKEN_RESPONSE_TYPE = "cloud_iam"
+REFRESH_TOKEN_GRANT_TYPE = "refresh_token"
+
+# Class to manage IAM Token Authentication
+class IAMTokenManager
+  attr_accessor :token_info, :user_access_token
+  def initialize(iam_api_key: nil, iam_access_token: nil, iam_url: nil)
+    @iam_api_key = iam_api_key
+    @user_access_token = iam_access_token
+    @iam_url = iam_url.nil? ? DEFAULT_IAM_URL : iam_url
+    @token_info = {
+      "access_token" => nil,
+      "refresh_token" => nil,
+      "token_type" => nil,
+      "expires_in" => nil,
+      "expiration" => nil
+    }
+  end
+
+  def request(method:, url:, headers: nil, params: nil, data: nil)
+    user_agent_string = "watson-apis-ruby-sdk-" + WatsonDeveloperCloud::VERSION
+    user_agent_string += " " + ENV["_system_name"]
+    user_agent_string += " " + ENV["_system_version"]
+    user_agent_string += " " + ENV["RUBY_VERSION"]
+    headers["User-Agent"] = user_agent_string
+    response = nil
+    if headers.key?("Content-Type") && headers["Content-Type"] == CONTENT_TYPE
+      response = HTTP.request(
+        method,
+        url,
+        body: HTTP::URI.form_encode(data),
+        headers: headers,
+        params: params
+      )
+    else
+      data = data.to_json if data.respond_to?(:to_json)
+      response = HTTP.request(
+        method,
+        url,
+        headers: headers,
+        body: data,
+        params: params
+      )
+    end
+    return JSON.parse(response.body.to_s) if (200..299).cover?(response.code)
+    require_relative("./watson_api_exception.rb")
+    raise WatsonApiException.new(response: response)
+  end
+
+  # The source of the token is determined by the following logic:
+  #   1. If user provides their own managed access token, assume it is valid and send it
+  #   2. If this class is managing tokens and does not yet have one, make a request for one
+  #   3. If this class is managing tokens and the token has expired refresh it. In case the refresh token is expired, get a new one
+  # If this class is managing tokens and has a valid token stored, send it
+  def _token
+    return @user_access_token unless @user_access_token.nil? || (@user_access_token.respond_to?(:empty?) && @user_access_token.empty?)
+    if @token_info.nil?
+      token_info = _request_token
+      _save_token_info(
+        token_info: token_info
+      )
+      return @token_info["access_token"]
+    elsif _is_token_expired?
+      token_info = _request_token if _is_refresh_token_expired?
+      token_info = _refresh_token unless _is_refresh_token_expired?
+      _save_token_info(
+        token_info: token_info
+      )
+      return @token_info["access_token"]
+    else
+      @token_info["access_token"]
+    end
+  end
+
+  # Request an IAM token using an API key
+  def _request_token
+    headers = {
+      "Content-Type" => CONTENT_TYPE,
+      "Authorization" => DEFAULT_AUTHORIZATION,
+      "Accept" => ACCEPT
+    }
+    data = {
+      "grant_type" => REQUEST_TOKEN_GRANT_TYPE,
+      "apikey" => @iam_api_key,
+      "response_type" => REQUEST_TOKEN_RESPONSE_TYPE
+    }
+    response = request(
+      method: "POST",
+      url: @iam_url,
+      headers: headers,
+      data: data
+    )
+    response
+  end
+
+  # Refresh an IAM token using a refresh token
+  def _refresh_token
+    headers = {
+      "Content-Type" => CONTENT_TYPE,
+      "Authorization" => DEFAULT_AUTHORIZATION,
+      "accept" => ACCEPT
+    }
+    data = {
+      "grant_type" => REFRESH_TOKEN_GRANT_TYPE,
+      "refresh_token" => @token_info["refresh_token"]
+    }
+    response = request(
+      method: "POST",
+      url: @iam_url,
+      headers: headers,
+      data: data
+    )
+    response
+  end
+
+  # Set a self-managed IAM access token.
+  # The access token should be valid and not yet expired.
+  def _access_token(iam_access_token:)
+    @user_access_token = iam_access_token
+  end
+
+  # Set the IAM api key
+  def _iam_api_key(iam_api_key:)
+    @iam_api_key = iam_api_key
+  end
+
+  # Check if currently stored token is expired.
+  # Using a buffer to prevent the edge case of the
+  # token expiring before the request could be made.
+  # The buffer will be a fraction of the total TTL. Using 80%.
+  def _is_token_expired?
+    fraction_of_ttl = 0.8
+    time_to_live = @token_info["expires_in"].nil? ? 0 : @token_info["expires_in"]
+    expire_time = @token_info["expiration"].nil? ? 0 : @token_info["expiration"]
+    refresh_time = expire_time - (time_to_live * (1.0 - fraction_of_ttl))
+    current_time = Time.now.to_i
+    refresh_time < current_time
+  end
+
+  # Used as a fail-safe to prevent the condition of a refresh token expiring,
+  # which could happen after around 30 days. This function will return true
+  # if it has been at least 7 days and 1 hour since the last token was set
+  def _is_refresh_token_expired?
+    return true if @token_info["expiration"].nil?
+    seven_days = 7 * 24 * 3600
+    current_time = Time.now.to_i
+    new_token_time = @token_info["expiration"] + seven_days
+    new_token_time < current_time
+  end
+
+  # Save the response from the IAM service request to the object's state
+  def _save_token_info(token_info:)
+    @token_info = token_info
+  end
+end
--- lib/watson_developer_cloud/watson_service.rb
+++ lib/watson_developer_cloud/watson_service.rb
@@ -5,8 +5,10 @@ require("erb")
 require("rubygems")
 require("http")
 require("stringio")
-require_relative("detailed_response")
-require_relative("watson_api_exception.rb")
+require_relative("./detailed_response.rb")
+require_relative("./watson_api_exception.rb")
+require_relative("./iam_token_manager.rb")
+require_relative("./version.rb")
 # require("httplog")
 # HttpLog.configure do |config|
 #   config.log_connect   = true
@@ -23,16 +25,48 @@ class WatsonService
   attr_accessor :url, :username, :password
   attr_reader :conn
   def initialize(vars)
+    defaults = {
+      vcap_services_name: nil,
+      username: nil,
+      password: nil,
+      use_vcap_services: true,
+      api_key: nil,
+      x_watson_learning_opt_out: false,
+      iam_api_key: nil,
+      iam_access_token: nil,
+      iam_url: nil
+    }
+    vars = defaults.merge(vars)
+    @url = vars[:url]
+    @api_key = vars[:api_key]
+    @username = vars[:username]
+    @password = vars[:password]
+    @iam_api_key = vars[:iam_api_key]
+    @iam_access_token = vars[:iam_access_token]
+    @iam_url = vars[:iam_url]
+    @token_manager = nil
+
+    user_agent_string = "watson-apis-ruby-sdk-" + WatsonDeveloperCloud::VERSION
+    user_agent_string += " " + ENV["_system_name"]
+    user_agent_string += " " + ENV["_system_version"]
+    user_agent_string += " " + ENV["RUBY_VERSION"]
+
+    headers = {
+      "User-Agent" => user_agent_string
+    }
+    headers["x-watson-learning-opt-out"] = true if vars[:x_watson_learning_opt_out]
+
+    if !vars[:api_key].nil?
+      _api_key(vars[:api_key])
+    elsif !vars[:username].nil? && !vars[:password].nil?
+      _set_username_and_password(username: vars[:username], password: vars[:password])
+    elsif !vars[:iam_access_token].nil? || !vars[:iam_api_key].nil?
+      _token_manager(iam_api_key: vars[:iam_api_key], iam_access_token: vars[:iam_access_token], iam_url: vars[:iam_url])
+    end
+
     @url = vars[:url] unless vars[:url].nil?
-    @username = vars[:username] unless vars[:username].nil?
-    @password = vars[:password] unless vars[:password].nil?
     @conn = HTTP::Client.new(
-      headers: {
-        "User-Agent" => "IBM-Ruby-SDK-Service"
-      }
-    ).basic_auth(
-      user: @username,
-      pass: @password
+      headers: headers
     ).timeout(
       :per_operation,
       read: 60,
@@ -41,9 +75,41 @@ class WatsonService
     )
   end
 
+  def _set_username_and_password(username: nil, password: nil)
+    @username = username unless username.nil?
+    @password = password unless password.nil?
+  end
+
   def add_default_headers(headers: {})
     raise TypeError unless headers.instance_of?(Hash)
     headers.each_pair { |k, v| @conn.default_options.headers.add(k, v) }
+  end
+
+  def _api_key(api_key:)
+    @api_key = api_key unless api_key.nil?
+  end
+
+  def _token_manager(iam_api_key: nil, iam_access_token: nil, iam_url: nil)
+    @iam_api_key = iam_api_key
+    @iam_access_token = iam_access_token
+    @iam_url = iam_url
+    @token_manager = IAMTokenManager.new(iam_api_key: iam_api_key, iam_access_token: iam_access_token, iam_url: iam_url)
+  end
+
+  def _iam_access_token(iam_access_token:)
+    @token_manager._access_token(iam_access_token) unless @token_manager.nil?
+    @token_manager = IAMTokenManager.new(iam_access_token: iam_access_token) if @token_manager.nil?
+    @iam_access_token = iam_access_token
+  end
+
+  def _iam_api_key(iam_api_key:)
+    @token_manager._iam_api_key(iam_api_key: iam_api_key) unless @token_manager.nil?
+    @token_manager = IAMTokenManager.new(iam_api_key: iam_api_key) if @token_manager.nil?
+    @iam_api_key = iam_api_key
+  end
+
+  def _url(url:)
+    @url = url
   end
 
   def request(args)
@@ -62,20 +128,33 @@ class WatsonService
     args[:params].delete_if { |_k, v| v.nil? } if args[:params].instance_of?(Hash)
     args.delete_if { |_, v| v.nil? }
     args[:headers].delete("Content-Type") if args.key?(:form) || args[:json].nil?
+
+    conn = @conn
+    unless @token_manager.nil?
+      access_token = @token_manager._token
+      args[:headers]["Authorization"] = "Bearer #{access_token}"
+    end
+    conn = @conn.basic_auth(user: @username, pass: @password) unless @username.nil? || @password.nil?
+    unless @api_key.nil?
+      args[:params] = {} if args[:params].nil?
+      args[:params]["apikey"] = @api_key if (@url + args[:url]).starts_with?("https://gateway-a.watsonplatform.net/calls")
+      args[:params]["api_key"] = @api_key unless (@url + args[:url]).starts_with?("https://gateway-a.watsonplatform.net/calls")
+    end
+
     if args.key?(:form)
-      response = @conn.follow.request(
+      response = conn.follow.request(
         args[:method],
         HTTP::URI.parse(@url + args[:url]),
-        headers: @conn.default_options.headers.merge(HTTP::Headers.coerce(args[:headers])),
+        headers: conn.default_options.headers.merge(HTTP::Headers.coerce(args[:headers])),
         params: args[:params],
         form: args[:form]
       )
     end
     unless args.key?(:form)
-      response = @conn.follow.request(
+      response = conn.follow.request(
         args[:method],
         HTTP::URI.parse(@url + args[:url]),
-        headers: @conn.default_options.headers.merge(HTTP::Headers.coerce(args[:headers])),
+        headers: conn.default_options.headers.merge(HTTP::Headers.coerce(args[:headers])),
         body: args[:json],
         params: args[:params]
       )
