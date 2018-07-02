1fe2caa622de18f59d5cc0c845137a97d392bf83
Max Nussbaum <maxnussbaum@live.com>
2018-07-02 16:10:57 -0400

    chore(language translator): Add example for language translator v3

---
+++ examples/language_translator_v3.rb
@@ -0,0 +1,54 @@
+require("watson_apis/language_translator_v3")
+
+language_translator = WatsonAPIs::LanguageTranslatorV3.new(
+  version: "2018-05-31",
+  ### url is optional, and defaults to the URL below. Use the correct URL for your region.
+  # url: "https://gateway.watsonplatform.net/language-translator/api",
+  iam_api_key: "your_api_key"
+)
+
+## Authenticate with username/password if your service instance doesn't provide an API key
+# language_translator = WatsonAPIs::LanguageTranslatorV3.new(
+#   version: "2018-05-31",
+#   username: "your_username",
+#   password: "your_password"
+# )
+
+## Translate
+translation = language_translator.translate(
+  text: "Hello",
+  model_id: "en-es"
+).body
+p translation
+
+## List identifiable languages
+# languages = language_translator.list_identifiable_languages.body
+# p languages
+
+## Identify
+# language = language_translator.identify(
+#   text: "Language translator translates text from one language to another"
+# ).body
+# p language
+
+## List models
+# models = language_translator.list_models(
+#   source: "en"
+# ).body
+# p models
+
+## Create model
+# glossary = File.open(Dir.getwd + "/resources/glossary.tmx")
+# response = language_translator.create_model(
+#   base_model_id: "en-es",
+#   name: "custom-english-to-spanish",
+#   forced_glossary: glossary
+# ).body
+# p response
+
+## Delete model
+# p language_translator.delete_model(model_id: "9f8d9c6f-2123-462f-9793-f17fdcb77cd6").body
+
+## Get model details
+# model = language_translator.get_model(model_id: "fdadfc3b-0b96-4276-a6e5-f5c4a29711fc").body
+# p model
