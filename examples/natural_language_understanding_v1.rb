855ba2d6fb5d649ca9198e0ae6c6b8713f015767
Max Nussbaum <maxnussbaum@live.com>
2018-07-03 08:26:37 -0400

    chore(nlu example): Remove unnecessary comments from example

--- examples/natural_language_understanding_v1.rb
+++ examples/natural_language_understanding_v1.rb
@@ -2,10 +2,8 @@ require("watson_apis/natural_language_understanding_v1")
 
 natural_language_understanding = WatsonAPIs::NaturalLanguageUnderstandingV1.new(
   version: "2018-03-16",
-  # username: "YOUR SERVICE USERNAME",
-  # password: "YOUR SERVICE PASSWORD"
-  username: ENV["NATURAL_LANGUAGE_UNDERSTANDING_USERNAME"],
-  password: ENV["NATURAL_LANGUAGE_UNDERSTANDING_PASSWORD"]
+  username: "YOUR SERVICE USERNAME",
+  password: "YOUR SERVICE PASSWORD"
 )
 
 response = natural_language_understanding.analyze(
