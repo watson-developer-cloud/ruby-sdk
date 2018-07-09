2926d593b4b1d8b1dfb6cd3db6fd82639f33f3e3
Max Nussbaum <maxnussbaum@live.com>
2018-07-09 12:11:02 -0400

    ci(test): Only run integration tests that have the proper credentials supplied

--- test/integration/test_language_translator_v3.rb
+++ test/integration/test_language_translator_v3.rb
@@ -4,78 +4,80 @@ require("json")
 require_relative("./../test_helper.rb")
 require("minitest/hooks/test")
 
-# Integration tests for the Language Translator V3 Service
-class LanguageTranslatorV3Test < Minitest::Test
-  include Minitest::Hooks
-  attr_accessor :service
-  def before_all
-    @service = IBMWatson::LanguageTranslatorV3.new(
-      username: ENV["LANGUAGE_TRANSLATOR_V3_USERNAME"],
-      password: ENV["LANGUAGE_TRANSLATOR_V3_PASSWORD"],
-      version: "2018-05-01"
-    )
-    @service.add_default_headers(
-      headers: {
-        "X-Watson-Learning-Opt-Out" => "1",
-        "X-Watson-Test" => "1"
-      }
-    )
-  end
+unless ENV["LANGUAGE_TRANSLATOR_V3_USERNAME"].nil? || ENV["LANGUAGE_TRANSLATOR_V3_PASSWORD"].nil?
+  # Integration tests for the Language Translator V3 Service
+  class LanguageTranslatorV3Test < Minitest::Test
+    include Minitest::Hooks
+    attr_accessor :service
+    def before_all
+      @service = IBMWatson::LanguageTranslatorV3.new(
+        username: ENV["LANGUAGE_TRANSLATOR_V3_USERNAME"],
+        password: ENV["LANGUAGE_TRANSLATOR_V3_PASSWORD"],
+        version: "2018-05-01"
+      )
+      @service.add_default_headers(
+        headers: {
+          "X-Watson-Learning-Opt-Out" => "1",
+          "X-Watson-Test" => "1"
+        }
+      )
+    end
 
-  def test_get_model
-    service_response = service.get_model(
-      model_id: "en-it"
-    ).result
-    refute(service_response.nil?)
-  end
+    def test_get_model
+      service_response = service.get_model(
+        model_id: "en-it"
+      ).result
+      refute(service_response.nil?)
+    end
 
-  def test_list_models
-    service_response = service.list_models.result
-    refute(service_response.nil?)
-  end
+    def test_list_models
+      service_response = service.list_models.result
+      refute(service_response.nil?)
+    end
 
-  def test_translate_source_target
-    service_response = service.translate(
-      text: "Hola, cómo estás? €",
-      source: "es",
-      target: "en"
-    ).result
-    refute(service_response.nil?)
-  end
+    def test_translate_source_target
+      service_response = service.translate(
+        text: "Hola, cómo estás? €",
+        source: "es",
+        target: "en"
+      ).result
+      refute(service_response.nil?)
+    end
 
-  def test_translate_model_id
-    service_response = service.translate(
-      text: "Messi is the best ever",
-      model_id: "en-es"
-    ).result
-    refute(service_response.nil?)
-  end
+    def test_translate_model_id
+      service_response = service.translate(
+        text: "Messi is the best ever",
+        model_id: "en-es"
+      ).result
+      refute(service_response.nil?)
+    end
 
-  def test_identify
-    service_response = service.identify(
-      text: "祝你有美好的一天"
-    ).result
-    refute(service_response.nil?)
-  end
+    def test_identify
+      service_response = service.identify(
+        text: "祝你有美好的一天"
+      ).result
+      refute(service_response.nil?)
+    end
 
-  def test_list_identifiable_languages
-    service_response = service.list_identifiable_languages.result
-    refute(service_response.nil?)
-  end
+    def test_list_identifiable_languages
+      service_response = service.list_identifiable_languages.result
+      refute(service_response.nil?)
+    end
 
-  def test_create_delete_model
-    skip "Methods not available in lite plans"
-    custom_model = File.open(Dir.getwd + "/resources/language_translator_model.tmx")
-    service_response = service.create_model(
-      base_model_id: "en-fr",
-      name: "test_glossary_ruby_integration",
-      forced_glossary: custom_model
-    ).result
-    refute(service_response.nil?)
-    model_id = service_response["model_id"]
-    service_response = service.delete_model(
-      model_id: model_id
-    ).result
-    assert_equal("OK", service_response["status"])
+    def test_create_delete_model
+      skip "Methods not available in lite plans"
+      custom_model = File.open(Dir.getwd + "/resources/language_translator_model.tmx")
+      service_response = service.create_model(
+        base_model_id: "en-fr",
+        name: "test_glossary_ruby_integration",
+        forced_glossary: custom_model
+      ).result
+      refute(service_response.nil?)
+      model_id = service_response["model_id"]
+      service_response = service.delete_model(
+        model_id: model_id
+      ).result
+      assert_equal("OK", service_response["status"])
+    end
   end
 end
--- test/integration/test_text_to_speech_v1.rb
+++ test/integration/test_text_to_speech_v1.rb
@@ -3,79 +3,81 @@
 require_relative("./../test_helper.rb")
 require("minitest/hooks/test")
 
-# Integration tests for the Text to Speech V1 Service
-class TextToSpeechV1Test < Minitest::Test
-  include Minitest::Hooks
-  attr_accessor :service
-  def before_all
-    @service = IBMWatson::TextToSpeechV1.new(
-      username: ENV["TEXT_TO_SPEECH_USERNAME"],
-      password: ENV["TEXT_TO_SPEECH_PASSWORD"]
-    )
-    @service.add_default_headers(
-      headers: {
-        "X-Watson-Learning-Opt-Out" => "1",
-        "X-Watson-Test" => "1"
-      }
-    )
-  end
+unless ENV["TEXT_TO_SPEECH_USERNAME"].nil? || ENV["TEXT_TO_SPEECH_PASSWORD"].nil?
+  # Integration tests for the Text to Speech V1 Service
+  class TextToSpeechV1Test < Minitest::Test
+    include Minitest::Hooks
+    attr_accessor :service
+    def before_all
+      @service = IBMWatson::TextToSpeechV1.new(
+        username: ENV["TEXT_TO_SPEECH_USERNAME"],
+        password: ENV["TEXT_TO_SPEECH_PASSWORD"]
+      )
+      @service.add_default_headers(
+        headers: {
+          "X-Watson-Learning-Opt-Out" => "1",
+          "X-Watson-Test" => "1"
+        }
+      )
+    end
 
-  def test_voices
-    output = @service.list_voices.result
-    refute(output["voices"].nil?)
-    voice = @service.get_voice(voice: output["voices"][0]["name"])
-    refute(voice.nil?)
-  end
+    def test_voices
+      output = @service.list_voices.result
+      refute(output["voices"].nil?)
+      voice = @service.get_voice(voice: output["voices"][0]["name"])
+      refute(voice.nil?)
+    end
 
-  def test_speak
-    output = @service.synthesize(
-      text: "my voice is my passport",
-      accept: "audio/wav",
-      voice: "en-US_AllisonVoice"
-    ).result
-    refute(output.nil?)
-  end
+    def test_speak
+      output = @service.synthesize(
+        text: "my voice is my passport",
+        accept: "audio/wav",
+        voice: "en-US_AllisonVoice"
+      ).result
+      refute(output.nil?)
+    end
 
-  def test_pronunciation
-    output = @service.get_pronunciation(
-      text: "hello"
-    ).result
-    refute(output["pronunciation"].nil?)
-  end
+    def test_pronunciation
+      output = @service.get_pronunciation(
+        text: "hello"
+      ).result
+      refute(output["pronunciation"].nil?)
+    end
 
-  def test_customizations
-    service_response = @service.list_voice_models
-    refute(service_response.nil?)
-  end
+    def test_customizations
+      service_response = @service.list_voice_models
+      refute(service_response.nil?)
+    end
 
-  def test_custom_words
-    customization_id = @service.create_voice_model(
-      name: "test_integration_customization",
-      description: "customization for tests"
-    ).result["customization_id"]
-    words = @service.list_words(customization_id: customization_id).result["words"]
-    assert(words.length.zero?)
-    @service.add_word(
-      customization_id: customization_id,
-      word: "ACLs",
-      translation: "ackles"
-    )
-    words = [{ "word" => "MACLs", "translation" => "mackles" }]
-    @service.add_words(
-      customization_id: customization_id,
-      words: words
-    )
-    @service.delete_word(
-      customization_id: customization_id,
-      word: "ACLs"
-    )
-    word = @service.get_word(
-      customization_id: customization_id,
-      word: "MACLs"
-    ).result
-    assert(word["translation"] == "mackles")
-    @service.delete_voice_model(
-      customization_id: customization_id
-    )
+    def test_custom_words
+      customization_id = @service.create_voice_model(
+        name: "test_integration_customization",
+        description: "customization for tests"
+      ).result["customization_id"]
+      words = @service.list_words(customization_id: customization_id).result["words"]
+      assert(words.length.zero?)
+      @service.add_word(
+        customization_id: customization_id,
+        word: "ACLs",
+        translation: "ackles"
+      )
+      words = [{ "word" => "MACLs", "translation" => "mackles" }]
+      @service.add_words(
+        customization_id: customization_id,
+        words: words
+      )
+      @service.delete_word(
+        customization_id: customization_id,
+        word: "ACLs"
+      )
+      word = @service.get_word(
+        customization_id: customization_id,
+        word: "MACLs"
+      ).result
+      assert(word["translation"] == "mackles")
+      @service.delete_voice_model(
+        customization_id: customization_id
+      )
+    end
   end
 end
