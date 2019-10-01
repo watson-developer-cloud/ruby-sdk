e41f79439e2fbe043f8af667aa0e15d98f960636
Mamoon Raja <mamoonraja1@gmail.com>
2019-10-01 08:29:51 -0400

    refactor: update examples to use authenticators

--- examples/assistant_v2.rb
+++ examples/assistant_v2.rb
@@ -1,20 +1,26 @@
 # frozen_string_literal: true
 
-require("ibm_watson/assistant_v2")
-require("json")
+require "json"
+require "ibm_watson/authenticators"
+require "ibm_watson/assistant_v2"
+
+# If using IAM
+authenticator = IBMWatson::Authenticators::IamAuthenticator.new(
+  apikey: "{iam_api_key}"
+)
+
+# If you have username & password in your credentials use:
+# authenticator = IBMWatson::Authenticators::BasicAuthenticator.new(
+#   username: "{username}",
+#   password: "{password}"
+# )
 
 # If you have username & password in your credentials use:
 service = IBMWatson::AssistantV2.new(
-  username: "YOUR SERVICE USERNAME",
-  password: "YOUR SERVICE PASSWORD",
+  authenticator: authenticator,
   version: "2018-09-17"
 )
-
-# If using IAM
-# assistant = IBMWatson::AssistantV1.new(
-#   iam_apikey: "IAM API KEY",
-#   version: "2018-09-17"
-# )
+service.service_url = "{service_url}"
 
 #########################
 # Sessions
--- examples/natural_language_understanding_v1.rb
+++ examples/natural_language_understanding_v1.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 
-require("ibm_watson/natural_language_understanding_v1")
-require("json")
+require "json"
+require "ibm_watson/authenticators"
+require "ibm_watson/natural_language_understanding_v1"
 
 # If using IAM
+authenticator = IBMWatson::Authenticators::IamAuthenticator.new(
+  apikey: "{iam_api_key}"
+)
+
+# If you have username & password in your credentials use:
+# authenticator = IBMWatson::Authenticators::BasicAuthenticator.new(
+#   username: "{username}",
+#   password: "{password}"
+# )
+
 natural_language_understanding = IBMWatson::NaturalLanguageUnderstandingV1.new(
-  iam_apikey: "IAM API KEY",
+  authenticator: authenticator,
   version: "2018-03-16"
 )
+natural_language_understanding.service_url = "{service_url}"
 
 # If you have username & password in your credentials use:
 # natural_language_understanding = IBMWatson::NaturalLanguageUnderstandingV1.new(
