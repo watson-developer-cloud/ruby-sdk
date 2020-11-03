331377ed96f6c6ec83b676531900c07b266721bb
Michael G Mosca <moscam@us.ibm.com>
2020-11-03 14:16:06 -0500

    Update personality_insights_v3.rb

--- lib/ibm_watson/personality_insights_v3.rb
+++ lib/ibm_watson/personality_insights_v3.rb
@@ -67,7 +67,7 @@ module IBMWatson
     #
     # @deprecated On 1 December 2021, Personality Insights will no longer be available.
     #   Consider migrating to Watson Natural Language Understanding.
-    #   For more information, see https://cloud.ibm.com/docs/natural-language-understanding.
+    #   For more information, see [Personality Insights Deprecation](https://github.com/watson-developer-cloud/ruby-sdk/tree/master#personality-insights-deprecation).
     def initialize(args = {})
       warn "On 1 December 2021, Personality Insights will no longer be available. For more information, see the README."
       @__async_initialized__ = false
