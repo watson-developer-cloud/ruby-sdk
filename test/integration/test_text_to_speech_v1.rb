770cd402935914998f7b2e00011b6552b424d529
Michael G Mosca <moscam@us.ibm.com>
2021-05-18 15:44:49 -0400

    feat: new tts tests

--- test/integration/test_text_to_speech_v1.rb
+++ test/integration/test_text_to_speech_v1.rb
@@ -83,6 +83,63 @@ if !ENV["TEXT_TO_SPEECH_APIKEY"].nil? && !ENV["TEXT_TO_SPEECH_URL"].nil?
         customization_id: customization_id
       )
     end
+
+    def test_custom_prompts
+      customization_id = @service.create_custom_model(
+        name: "test_integration_customization",
+        description: "RUBY customization for tests"
+      ).result["customization_id"]
+
+      prompt_metadata = { prompt_text: "Thank you and goodbye" }
+      prompt_json = JSON[prompt_metadata]
+      audio_file = File.open(Dir.getwd + "/resources/speech.wav")
+      prompt = @service.add_custom_prompt(
+        customization_id: customization_id,
+        prompt_id: "rubyprompt_id",
+        metadata: prompt_json,
+        file: audio_file
+      ).result["prompt"]
+      assert(prompt == "Thank you and goodbye")
+
+      prompts = @service.list_custom_prompts(customization_id: customization_id).result["prompts"]
+      refute(prompts.nil?)
+
+      prompt = @service.get_custom_prompt(
+        customization_id: customization_id,
+        prompt_id: "rubyprompt_id"
+      ).result["prompt"]
+      assert(prompt == "Thank you and goodbye")
+
+      @service.delete_custom_prompt(
+        customization_id: customization_id,
+        prompt_id: "rubyprompt_id"
+      )
+
+      @service.delete_custom_model(
+        customization_id: customization_id
+      )
+    end
+
+    def test_speaker_models
+      audio_file = File.open(Dir.getwd + "/resources/speech.wav")
+      @service.create_speaker_model(
+        speaker_name: "RubyMike",
+        audio: audio_file
+      ).result["speaker_model"]
+
+      speakers = @service.list_speaker_models.result["speakers"]
+      speaker_id = speakers[0]["speaker_id"]
+      # puts JSON.pretty_generate(speakers)
+
+      @service.get_speaker_model(
+        speaker_id: speaker_id
+      ).result["models_speaker"]
+
+      # speaker_id = d3d03b69-4035-4420-928d-7ac2e0249829
+      @service.delete_speaker_model(
+        speaker_id: speaker_id
+      )
+    end
   end
 else
   class TextToSpeechV1Test < Minitest::Test
