0329ad909e1aaeac068493fd6d77f0da03d3bab0
Michael G Mosca <moscam@us.ibm.com>
2021-05-19 08:45:14 -0400

    feat: unit tests for NLU

--- test/unit/test_natural_language_understanding_v1.rb
+++ test/unit/test_natural_language_understanding_v1.rb
@@ -144,4 +144,235 @@ class NaturalLanguageUnderstandingV1Test < Minitest::Test
     )
     assert_equal({ "deleted" => model_id }, service_response.result)
   end
+
+  def test_sentiment_models
+    authenticator = IBMWatson::Authenticators::BasicAuthenticator.new(
+      username: "username",
+      password: "password"
+    )
+    service = IBMWatson::NaturalLanguageUnderstandingV1.new(
+      version: "2018-03-16",
+      authenticator: authenticator
+    )
+
+    stub_request(:post, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/sentiment?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    training_data = File.open(Dir.getwd + "/resources/weather_data_train.csv")
+    service_response = service.create_sentiment_model(
+      language: "en",
+      training_data: training_data
+    )
+    assert_equal({ "resulting_key" => true }, service_response.result)
+
+    stub_request(:get, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/sentiment?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    service_response = service.list_sentiment_models
+    assert_equal({ "resulting_key" => true }, service_response.result)
+
+    stub_request(:get, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/sentiment/model_id?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    service_response = service.get_sentiment_model(model_id: "model_id")
+    assert_equal({ "resulting_key" => true }, service_response.result)
+
+    stub_request(:put, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/sentiment/model_id?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    training_data = File.open(Dir.getwd + "/resources/weather_data_train.csv")
+    service_response = service.update_sentiment_model(
+      model_id: "model_id",
+      language: "en",
+      training_data: training_data
+    )
+    assert_equal({ "resulting_key" => true }, service_response.result)
+
+    stub_request(:delete, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/sentiment/model_id?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    service_response = service.delete_sentiment_model(
+      model_id: "model_id"
+    )
+    assert_equal({ "resulting_key" => true }, service_response.result)
+  end
+
+  def test_categories_models
+    authenticator = IBMWatson::Authenticators::BasicAuthenticator.new(
+      username: "username",
+      password: "password"
+    )
+    service = IBMWatson::NaturalLanguageUnderstandingV1.new(
+      version: "2018-03-16",
+      authenticator: authenticator
+    )
+
+    stub_request(:post, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/categories?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    training_data = File.open(Dir.getwd + "/resources/weather_data_train.csv")
+    service_response = service.create_categories_model(
+      language: "en",
+      training_data: training_data
+    )
+    assert_equal({ "resulting_key" => true }, service_response.result)
+
+    stub_request(:get, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/categories?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    service_response = service.list_categories_models
+    assert_equal({ "resulting_key" => true }, service_response.result)
+
+    stub_request(:get, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/categories/model_id?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    service_response = service.get_categories_model(model_id: "model_id")
+    assert_equal({ "resulting_key" => true }, service_response.result)
+
+    stub_request(:put, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/categories/model_id?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    training_data = File.open(Dir.getwd + "/resources/weather_data_train.csv")
+    service_response = service.update_categories_model(
+      model_id: "model_id",
+      language: "en",
+      training_data: training_data
+    )
+    assert_equal({ "resulting_key" => true }, service_response.result)
+
+    stub_request(:delete, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/categories/model_id?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    service_response = service.delete_categories_model(
+      model_id: "model_id"
+    )
+    assert_equal({ "resulting_key" => true }, service_response.result)
+  end
+
+  def test_classifications_models
+    authenticator = IBMWatson::Authenticators::BasicAuthenticator.new(
+      username: "username",
+      password: "password"
+    )
+    service = IBMWatson::NaturalLanguageUnderstandingV1.new(
+      version: "2018-03-16",
+      authenticator: authenticator
+    )
+
+    stub_request(:post, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/classifications?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    training_data = File.open(Dir.getwd + "/resources/weather_data_train.csv")
+    service_response = service.create_classifications_model(
+      language: "en",
+      training_data: training_data
+    )
+    assert_equal({ "resulting_key" => true }, service_response.result)
+
+    stub_request(:get, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/classifications?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    service_response = service.list_classifications_models
+    assert_equal({ "resulting_key" => true }, service_response.result)
+
+    stub_request(:get, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/classifications/model_id?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    service_response = service.get_classifications_model(model_id: "model_id")
+    assert_equal({ "resulting_key" => true }, service_response.result)
+
+    stub_request(:put, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/classifications/model_id?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    training_data = File.open(Dir.getwd + "/resources/weather_data_train.csv")
+    service_response = service.update_classifications_model(
+      model_id: "model_id",
+      language: "en",
+      training_data: training_data
+    )
+    assert_equal({ "resulting_key" => true }, service_response.result)
+
+    stub_request(:delete, "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/v1/models/classifications/model_id?version=2018-03-16")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Authorization" => "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+          "Host" => "api.us-south.natural-language-understanding.watson.cloud.ibm.com"
+        }
+      ).to_return(status: 200, body: { resulting_key: true }.to_json, headers: { "Content-Type" => "application/json" })
+    service_response = service.delete_classifications_model(
+      model_id: "model_id"
+    )
+    assert_equal({ "resulting_key" => true }, service_response.result)
+  end
 end
